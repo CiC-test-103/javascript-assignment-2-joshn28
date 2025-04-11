68bd2bbc513268283d9dc8e310b14786d6584b2d
Joshua Nobleza <joshnobleza@bell.net>
2025-04-11 17:59:05 -0400

    Implement Banking System

--- Bank.js
+++ Bank.js
@@ -7,7 +7,11 @@ class Bank {
 
     // Add methods here:
     // Example: createAccount(name, initialDeposit)
-
+    createAccount(name, initialDeposit) {
+        const newAccount = new Account(name, initialDeposit);
+        this.accounts.push(newAccount);
+        return newAccount;
+    }
 }
 
 // Account Class: Represents a single user's account
@@ -22,15 +26,57 @@ class Account {
     // Example: deposit(amount) 
     // example data to be stored in transactionHistory { transactionType: 'Deposit', amount: 500 }
 
+    deposit(amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            this.transactionHistory.push({
+                transactionType: 'Deposit',
+                amount: amount
+            });
+        }
+    }
+
     // Example: withdraw(amount)
     // example data to be stored in transactionHistory { transactionType: 'Withdrawal', amount: 200 }
+
+    withdraw(amount) {
+        if (this.balance - amount >= 0 && amount > 0) {
+            this.balance -= amount;
+            this.transactionHistory.push({
+                transactionType: 'Withdrawal',
+                amount: amount
+            });
+        }
+    }
 
     // Example: transfer(amount, recipientAccount)
     // example data to be stored in transactionHistory:
     // for account sending { transactionType: 'Transfer', amount: 300, to: recipientName }
     // for account recieving { transactionType: 'Received', amount: 300, from: senderName }
-    
+
+    transfer(amount, recipientAccount) {
+        if (this.balance - amount >= 0 && amount > 0) {
+            this.balance -= amount;
+            this.transactionHistory.push({
+                transactionType: 'Transfer',
+                amount: amount,
+                to: recipientAccount.name
+            });
+
+            recipientAccount.balance += amount;
+            recipientAccount.transactionHistory.push({
+                transactionType: 'Received',
+                amount: amount,
+                from: this.name
+            });
+        }
+    }
+
     // Example: checkBalance()
+
+    checkBalance() {
+        return this.balance;
+    }
 }
 
 //<-------------------------------DO NOT WRITE BELOW THIS LINE------------------------------>
